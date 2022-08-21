d678a34776107438db4cda61b27969ff09e53008
Enjoy3325 <enjoy3325@gmail.com>
2022-08-21 17:39:16 +0300

    03-08-hw-js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,14 @@ const videoIframe = document.querySelector('iframe');
 
 const player = new Player(videoIframe);
 
-player.on('play', function () {
+player.on('play', function (e) {
+  e.preventDefault();
   console.log('played the video!');
 });
 
 // 1) Понять что от меня хотят
 // 2) Скачать и подключить библиотеку
 // 3) Подключил импорт
-// 4) Вытянул с HTM в JS iframe тег
+// 4) Вытянул с HTML в JS iframe тег
 // 5) Прописать функцию с on('')
 //
