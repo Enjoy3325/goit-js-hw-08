ebe7000c47e25b0786781980d9f2e408d55d07c1
Enjoy3325 <enjoy3325@gmail.com>
2022-08-23 01:04:28 +0300

    well-done-08-js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,61 +1,53 @@
 import throttle from 'lodash.throttle';
 
-// var throttle = require('lodash.throttle');
-
-// - Вынес имя Константы, избавился от магических строк
-const STORAGE_KEY = 'feedback-form-state';
-// Хранение в разных объектах
-const formData = {};
-
 const refs = {
   form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form  textarea'),
+  textarea: document.querySelector('textarea'),
+  input: document.querySelector('input'),
 };
-refs.form.addEventListener('sumbit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
-// - Делегирование. На форму повесил прослушивание input,
-//  Сделал разными єлемментами под таргет
-// ! - Сделать так чтобы сохраняло не только сообщение но и email и всё в одном объекте
-refs.form.addEventListener('input', evt => {
-  // console.log(evt.target.name);
-  // console.log(evt.target.value);
+const STORAGE_KEY = 'feedback-form-state';
 
-  formData[evt.target.name] = evt.target.value;
-  console.log(formData);
-});
-populateTextarea();
-// - Останавливаем поведение по умолчанию
-// - Убираем сообщение из хранилища
-// - Очищаем форму e.target.reset
+document.addEventListener('DOMContentLoaded', setInputs);
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(setLocalStorage, 500));
+
+let formDataParse = null;
+setInputs();
+
+const formData = {
+  email: formDataParse?.email ? formDataParse.email : '',
+  message: formDataParse?.message ? formDataParse.message : '',
+};
+
+function setLocalStorage(e) {
+  let valueInput = e.target.value;
+  let nameInput = e.target.name;
+
+  formData[nameInput] = valueInput;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function setInputs() {
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
+  formDataParse = JSON.parse(savedMessage);
+
+  if (formDataParse) {
+    if (formDataParse.email) {
+      refs.input.value = formDataParse.email;
+    }
+    if (formDataParse.message) {
+      refs.textarea.value = formDataParse.message;
+    }
+  }
+}
 
 function onFormSubmit(e) {
   e.preventDefault();
-  // console.log('Отправляем форму');
-  e.target.reset();
-  // - Чистит из localStorage старое сообщение методом .removeItem и в ('ключ')
-  localStorage.removeItem(STORAGE_KEY);
-}
-
-// - Получаем значение поля при вводе в textarea
-// - Сохраняем его в хранилище
-// - Добавить throttle
-
-function onTextareaInput(e) {
-  const message = e.target.value;
-  // message является уже строка потому не нужно записывать через JSON ''
-  localStorage.setItem(STORAGE_KEY, message);
-}
-
-// - Получаем значение из хранилища
-// - Если там что-то было, обновлякм DOM
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-  //Если savedMessage есть то будет тру, то мы можем с ним работать,
-  // там какие - то данные, в противном случае ничего не делаем
-  if (savedMessage) {
-    console.log(savedMessage);
-    // - Берём refs.textarea и записываем ей .value
-    refs.textarea.value = savedMessage;
+  if (!formData.email || !formData.message) {
+    alert('Заповніть всі поля, будь-ласка');
   }
+  refs.form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
 }
